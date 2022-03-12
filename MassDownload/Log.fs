b371bec44345ee16fb5dd7aca8605fff2ee03844
biggskanz <biggskanz@gmail.com>
2022-03-11 23:26:04 -0600

    Initial commit, synchronous downloader

---
+++ MassDownload/Log.fs
@@ -0,0 +1,22 @@
+﻿namespace MassDownload
+
+module Log =
+    
+    open System
+    open System.Threading
+    
+    /// Print a colored log message
+    let message (color : ConsoleColor) (message : string ) =
+        Console.ForegroundColor <- color
+        printfn "%s (thread ID: %i)"
+            message Thread.CurrentThread.ManagedThreadId
+        Console.ResetColor()
+    
+    /// Print a red log message.
+    let red = message ConsoleColor.Red
+    let green = message ConsoleColor.Green
+    let yellow = message ConsoleColor.Yellow
+    let cyan  = message ConsoleColor.Cyan
+    
+        
+    
